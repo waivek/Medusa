7c69186c7c3c5826d8c4fd470edcc3969af941f4
waivek <waivek@gmail.com>
2016-08-04 21:16:23 +0530

    Initial Commit

---
+++ src/Player.py
@@ -0,0 +1,36 @@
+import pygame
+
+class Player():
+    def __init__(self, path_to_sprite, default_speed):
+        self.position = (0, 0)
+        self.velocity = (0, 0)
+        self.default_speed = default_speed
+        self.sprite = pygame.image.load(path_to_sprite)
+        self.sprite_rect = self.sprite.get_rect()
+
+    def draw(self, screen):
+        screen.blit(self.sprite, self.sprite_rect)
+
+    def move(self, displacement):
+        self.sprite_rect = self.sprite_rect.move(displacement)
+        newX = self.position[0] + displacement[0]
+        newY = self.position[1] + displacement[1]
+        self.position = (newX, newY)
+
+    def handleEvent(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.velocity = (-self.default_speed, self.velocity[1])
+            if event.key == pygame.K_RIGHT:
+                self.velocity = (self.default_speed, self.velocity[1])
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                self.velocity = (0, self.velocity[1])
+
+    def Update(self, deltaTime):
+        dt = deltaTime / 1000
+        print("deltaTime %d\ndt %f" %(deltaTime, dt))
+        self.move((self.velocity[0] * dt, self.velocity[1] * dt))
+
+
+
