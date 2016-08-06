24dc2a6f6c119c2af3e8011fffa4701d78167f58
waivek <waivek@gmail.com>
2016-08-06 16:13:51 +0530

    Added player and level to main function
    Main calls level update in it's Update loop. Level then does the same
    for the players inside Level

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from src.Level import Level
 from src.Player import Player
 
 def main():
@@ -8,21 +9,25 @@ def main():
     size = width, height = 800, 400
     screen = pygame.display.set_mode(size)
     black = 0, 0, 0
-    p1 = Player(path_to_sprite="..\\raw\\sprite.jpg", default_speed=100)
-    print(p1.__class__)
+    level = Level(20, 100)
+    p1 = Player(path_to_sprite="..\\raw\\sprite.png", default_speed=100)
+    level.add_player(p1)
     time = pygame.time.get_ticks()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
-            p1.handleEvent(event)
+            # p1.handleEvent(event)
+            level.handle_event(event)
         screen.fill(black)
         deltaTime = pygame.time.get_ticks() - time
         while deltaTime < 16:
             deltaTime = pygame.time.get_ticks() - time
         time = pygame.time.get_ticks()
-        p1.Update(deltaTime)
-        p1.draw(screen)
+        # p1.Update(deltaTime)
+        # p1.draw(screen)
+        level.Update(deltaTime)
+        level.draw(screen)
         pygame.display.flip()
 
 main()
